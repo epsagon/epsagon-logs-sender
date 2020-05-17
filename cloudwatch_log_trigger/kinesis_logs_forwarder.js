c54e40470b60cebac4c51fc2cc4d5918bf69f0eb
Tal Vintrob <tal@epsagon.com>
2020-05-17 17:09:05 +0300

    kinesis logs sender

--- cloudwatch_log_trigger/kinesis_logs_forwarder.js
+++ cloudwatch_log_trigger/kinesis_logs_forwarder.js
@@ -6,8 +6,9 @@ const util = require("util");
 const zlib = require("zlib");
 const AWS = require("aws-sdk");
 
-const gunzip = util.promisify(zlib.gunzip);
 const kinesisClient = new AWS.Kinesis({ region: process.env.EPSAGON_REGION });
+const gunzip = util.promisify(zlib.gunzip);
+const putRecords = util.promisify(kinesisClient.putRecords);
 
 const epsagonToken = process.env.EPSAGON_TOKEN;
 const userLogsKinesis = process.env.EPSAGON_LOGS_KINESIS;
@@ -29,41 +30,45 @@ function createRecordData(logEvents, transformEvent) {
 function createRecord(logsData) {
   return {
     Data: createRecordData(logsData.logEvents),
-    PartitionKey: logsData.logStream,
+    PartitionKey: epsagonToken,
   };
 }
 
-function mapToEpsagonRecord(record) {
+async function mapToEpsagonRecord(record) {
   const data = record.kinesis.data;
-  const zippedBuf = new Buffer.from(data, "base64");
+  const decoded = new Buffer.from(data, "base64").toString("utf-8");
+  const unzipped = await gunzip(decoded);
+  const logsData = JSON.parse(unzipped);
 
-  return gunzip(zippedBuf)
-    .then((unzipped) => JSON.parse(unzipped.toString("utf-8")))
-    .then((logsData) => logsData.messageType === "DATA_MESSAGE" && logsData)
-    .then((logsData) => logsData && createRecord(logsData))
-    .then((logsData) => {
-      if (!logsData) {
-        epsagon_debug("could not extract data, check message type");
-      }
+  if (logsData.messageType !== "DATA_MESSAGE") {
+    epsagon_debug("invalid messageType");
+    return;
+  }
 
-      return logsData;
-    });
+  return createRecord(logsData);
 }
 
-function forwardLambdaHandler(event, _) {
-  const records = Promise.all(event.Records.map(mapToEpsagonRecord));
-  records.then((records) => {
-    kinesisClient.putRecords(
-      { Records: records, StreamName: userLogsKinesis },
-      function (err, data) {
-        epsagon_debug("Record sent");
-        epsagon_debug(data);
-        if (err) {
-          epsagon_debug(err);
-        }
-      }
-    );
-  });
+async function forwardLambdaHandler(event, _) {
+  const records = await Promise.all(event.Records.map(mapToEpsagonRecord));
+  const filtered = records.filter((record) => record);
+
+  epsagon_debug(records);
+  epsagon_debug(userLogsKinesis);
+
+  if (filtered.length === 0) return;
+
+  try {
+    const data = await putRecords({
+      Records: filtered,
+      StreamName: userLogsKinesis,
+    });
+
+    epsagon_debug("Record sent");
+    epsagon_debug(data);
+  } catch (e) {
+    epsagon_debug(e);
+    return;
+  }
 }
 
 module.exports = { forwardLambdaHandler };
